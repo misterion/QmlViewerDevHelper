2223dd1084891ea18bc6608a88e748d065b56110
Николай Бондаренко <nikolay.bondarenko@syncopate.ru>
2012-11-01 19:18:33 +0400

    Initial commit

---
+++ QmlViewerDevHelper/version.h
@@ -0,0 +1,51 @@
+/****************************************************************************
+** This file is a part of Syncopate Limited GameNet Application or it parts.
+**
+** Copyright (©) 2011 - 2012, Syncopate Limited and/or affiliates. 
+** All rights reserved.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+****************************************************************************/
+
+#ifndef _GGS_AUTORUNHELPER_VERSION_H_
+#define _GGS_AUTORUNHELPER_VERSION_H_
+
+#ifndef HUDSON_BUILD
+    #define FILEVER          1,0,0,0
+    #define PRODUCTVER       FILEVER
+    #define STRFILEVER       "1,0,0,0"
+    #define STRPRODUCTVER    STRFILEVER
+
+    #define COMPANYNAME      "GGS"
+    #define FILEDESCRIPTION  "Developer version of QmlViewerDevHelper"
+    #define INTERNALNAME     "QmlViewerDevHelper"
+    #define LEGALCOPYRIGHT   "Copyright(c) 2012"
+
+    #ifdef DEBUG 
+        #define ORIGINALFILENAME "QmlViewerDevHelperX86d.dll"
+    #else
+        #define ORIGINALFILENAME "QmlViewerDevHelperX86.dll"
+    #endif
+
+    #define PRODUCTNAME      "QmlViewerDevHelper"
+#else
+    #define FILEVER          $$MAJOR$$,$$MINOR$$,$$HUDSON_BUILD$$
+    #define PRODUCTVER       FILEVER
+    #define STRFILEVER       "$$MAJOR$$,$$MINOR$$,$$HUDSON_BUILD$$,$$GIT_REVISION$$"
+    #define STRPRODUCTVER    STRFILEVER
+
+    #define COMPANYNAME      "$$COMPANYNAME$$"
+    #define FILEDESCRIPTION  "$$FILEDESCRIPTION$$"
+    #define INTERNALNAME     "$$INTERNALNAME$$"
+    #define LEGALCOPYRIGHT   "$$LEGALCOPYRIGHT$$"
+    #define ORIGINALFILENAME "$$FILENAME$$"
+    #define PRODUCTNAME      "$$PRODUCTNAME$$"
+#endif
+
+#endif //_GGS_AUTORUNHELPER_VERSION_H_
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 QmlViewerDevHelper
 ==================
+
+How to set the the path of images in qml file if the qml file is in resource?
+(http://qt-project.org/forums/viewthread/7047). The answer is:
+
+  QString imagePath = QDir::currentPath()+"/images/";
+  #if defined(Q_OS_MAC)
+  imagePath = "file://"+imagePath;
+  #endif
+  QDeclarativeContext::setContextProperty("imagePath",imagePath);
+
+and in qml 
+
+   Image {
+   source: imagePath + "image.png"  
+   }
+
+in your c++ application. But it`s became a real pain in QtCreator Qml editor and
+qml viewer. Yes, you can write in manualy with 
+  property string imagePath: ...
+
+but it`s not clear and make some troubles with Loader. 
+
+Jusn build project and put the dll to your qmlviewer plugin folder. 
+Also you need to put valid qmldir file:
+plugin QmlViewerDevHelperX86
+
+You can put dll in any folder you like, just put the path with -I options 
+on QmlViewer. 
+
+And usage:
+-I "/var varName=varValue" 
+or 
+-P "/var varName=varValue" 
+so varName would be available in qml. 
+       
+(c) 2012 Nikolay Bondarenko <misterionkell@gmail.com>
